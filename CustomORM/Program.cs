cf1f8b047fa7d2294eb4942cd6f6b80abd7f3cf4
Роман Ищенко <mahodzi@gmail.com>
2019-12-21 17:39:12 +0200

    Add project files.

---
+++ CustomORM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomORM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
